26e45c65a135e1b9433bb569fe80e1d6d1928667
sebastianiv21 <seluti98@gmail.com>
2023-11-23 21:02:29 -0500

    chore: add assets

--- src/components/ui/Spinner.tsx
+++ src/components/ui/Spinner.tsx
@@ -2,11 +2,11 @@ import { type FC } from "react"
 import { Spinner } from "react-bootstrap"
 
 const CustomSpinner: FC = () => (
-  <div className="d-flex justify-content-center gap-2">
-    <Spinner animation="grow" variant="primary" />
-    <Spinner animation="grow" variant="primary" />
-    <Spinner animation="grow" variant="primary" />
-  </div>
+  <section className="d-flex justify-content-center gap-2">
+    <Spinner animation="grow" variant="secondary" />
+    <Spinner animation="grow" variant="secondary" />
+    <Spinner animation="grow" variant="secondary" />
+  </section>
 )
 
 export default CustomSpinner
