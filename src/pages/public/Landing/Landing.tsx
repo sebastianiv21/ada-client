0f16b9c1383df44632a391bc1475607f0c7e35af
sebastianiv21 <seluti98@gmail.com>
2023-11-24 00:45:20 -0500

    feat: setup nested routes

---
+++ src/pages/public/Landing/Landing.tsx
@@ -0,0 +1,8 @@
+import { type FC } from "react"
+import { Container } from "react-bootstrap"
+
+const Landing: FC = () => {
+  return <Container>Landing: FC</Container>
+}
+
+export default Landing
---
+++ src/pages/public/components/Footer/Footer.tsx
@@ -0,0 +1,7 @@
+import { type FC } from "react"
+
+const Footer: FC = () => {
+  return <footer>Footer: FC</footer>
+}
+
+export default Footer
