1c1cc6aaa453943558d370c772a7fc74c3751d45
sebastianiv21 <seluti98@gmail.com>
2023-11-24 17:51:56 -0500

    style: main element
    create new contact card component

--- src/routes/PublicRoutes.tsx
+++ src/routes/PublicRoutes.tsx
@@ -5,18 +5,23 @@ import Footer from "@/pages/public/components/Footer/Footer"
 import Landing from "@/pages/public/Landing/Landing"
 import NotFound from "@/pages/NotFound"
 import NestedRoutes from "@/components/NestedRoutes"
+import { Container } from "react-bootstrap"
+import Contact from "@/pages/public/Contact/Contact"
 
 const PublicRoutes: FC = () => {
   return (
     <Route path="/:any*">
       <NestedRoutes base="">
         <Navbar />
-        <Switch>
-          <Route path="/" component={Landing} />
-          <Route component={NotFound} />
-        </Switch>
+        <Container>
+          <Switch>
+            <Route path="/" component={Landing} />
+            <Route path="/contacto" component={Contact} />
+            <Route component={NotFound} />
+          </Switch>
+        </Container>
+        <Footer />
       </NestedRoutes>
-      <Footer />
     </Route>
   )
 }
