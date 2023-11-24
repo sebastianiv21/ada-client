096bc264ed2bc2ca36dd2a4a671fc306b7c55a0c
sebastianiv21 <seluti98@gmail.com>
2023-11-24 06:40:36 -0500

    feat: add landing page

---
+++ src/pages/public/Landing/components/MainInfo.tsx
@@ -0,0 +1,30 @@
+import { type FC } from "react"
+import { Button, Image, Stack } from "react-bootstrap"
+
+const MainInfo: FC = () => {
+  return (
+    <Stack direction="vertical" gap={4}>
+      <Stack direction="horizontal" gap={4}>
+        <Image src="/logo.png" width={150} />
+        <Stack direction="vertical" className="justify-content-center">
+          <h1>ADA Health</h1>
+          <h2>Sistema Hospitalario</h2>
+        </Stack>
+      </Stack>
+      <h4>
+        El sistema de información para administrar las pruebas de adenosina
+        desaminasa en los hospitales del departamento del Huila.
+      </h4>
+      <Button
+        variant="secondary"
+        size="lg"
+        href="#landing-about"
+        className="me-auto rounded-pill"
+      >
+        Acerca del examen
+      </Button>
+    </Stack>
+  )
+}
+
+export default MainInfo
