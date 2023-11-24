1297ac0fa81016fe2dd642ff1881f75c5ab78d86
sebastianiv21 <seluti98@gmail.com>
2023-11-23 20:43:41 -0500

    chore: add double quotes

--- src/components/ui/Spinner.tsx
+++ src/components/ui/Spinner.tsx
@@ -1,5 +1,5 @@
-import { type FC } from 'react'
-import { Spinner } from 'react-bootstrap'
+import { type FC } from "react"
+import { Spinner } from "react-bootstrap"
 
 const CustomSpinner: FC = () => (
   <div className="d-flex justify-content-center gap-2">
