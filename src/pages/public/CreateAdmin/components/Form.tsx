86ced42e50274b28644ce48b1096c1b5eb72b227
sebastianiv21 <seluti98@gmail.com>
2023-12-01 21:01:34 -0500

    feat: add registration

--- src/pages/public/CreateAdmin/components/Form.tsx
+++ src/pages/public/CreateAdmin/components/Form.tsx
@@ -4,8 +4,9 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons"
 import useToggle from "@/hooks/useToggle"
 import { type AdminFormData, adminSchema } from "@/types/usuarioTypes"
-import { useForm } from "react-hook-form"
+import { type SubmitHandler, useForm } from "react-hook-form"
 import { yupResolver } from "@hookform/resolvers/yup"
+import { format } from "date-fns"
 
 const CreateAdminForm: FC = () => {
   const [showPassword, toggleShowPassword] = useToggle()
@@ -20,17 +21,20 @@ const CreateAdminForm: FC = () => {
   })
 
   // TODO: Add submit handler
-  const onSubmit = async (data: AdminFormData): void => {
+  const onSubmit: SubmitHandler<AdminFormData> = async (data) => {
     console.log(data)
     // reset()
   }
 
   return (
-    <Form onSubmit={handleSubmit(onSubmit)}>
+    <Form noValidate onSubmit={handleSubmit(onSubmit)}>
       <Row className="mb-3">
         <Form.Group as={Col} md="4" controlId="validationCustom01">
           <Form.Label>Tipo de documento</Form.Label>
-          <Form.Select {...register("tipoDocumento")}>
+          <Form.Select
+            {...register("tipoDocumento")}
+            isInvalid={errors.tipoDocumento}
+          >
             <option value="">Seleccione</option>
             <option value="1">Peticiones</option>
             <option value="2">Quejas</option>
@@ -38,7 +42,7 @@ const CreateAdminForm: FC = () => {
             <option value="4">Sugerencias</option>
           </Form.Select>
           <Form.Control.Feedback type="invalid">
-            Looks good!
+            {errors.tipoDocumento?.message}
           </Form.Control.Feedback>
         </Form.Group>
 
@@ -46,11 +50,12 @@ const CreateAdminForm: FC = () => {
           <Form.Label>Número de documento</Form.Label>
           <Form.Control
             {...register("numeroDocumento")}
+            isInvalid={errors.numeroDocumento}
             type="text"
             placeholder="Ingrese su número de documento"
           />
           <Form.Control.Feedback type="invalid">
-            Looks good!
+            {errors.numeroDocumento?.message}
           </Form.Control.Feedback>
         </Form.Group>
 
@@ -58,11 +63,12 @@ const CreateAdminForm: FC = () => {
           <Form.Label>Nombres</Form.Label>
           <Form.Control
             {...register("nombres")}
+            isInvalid={errors.nombres}
             type="text"
             placeholder="Ingrese sus nombres"
           />
           <Form.Control.Feedback type="invalid">
-            Looks good!
+            {errors.nombres?.message}
           </Form.Control.Feedback>
         </Form.Group>
       </Row>
@@ -72,11 +78,12 @@ const CreateAdminForm: FC = () => {
           <Form.Label>Apellidos</Form.Label>
           <Form.Control
             {...register("apellidos")}
+            isInvalid={errors.apellidos}
             type="text"
             placeholder="Ingrese sus apellidos"
           />
           <Form.Control.Feedback type="invalid">
-            Looks good!
+            {errors.apellidos?.message}
           </Form.Control.Feedback>
         </Form.Group>
 
@@ -84,17 +91,19 @@ const CreateAdminForm: FC = () => {
           <Form.Label>Fecha de nacimiento</Form.Label>
           <Form.Control
             {...register("fechaNacimiento")}
+            isInvalid={errors.fechaNacimiento}
             type="date"
+            defaultValue={format(new Date(), "yyyy-MM-dd")}
             placeholder="Ingrese su fecha de nacimiento"
           />
           <Form.Control.Feedback type="invalid">
-            Looks good!
+            {errors.fechaNacimiento?.message}
           </Form.Control.Feedback>
         </Form.Group>
 
         <Form.Group as={Col} md="4" controlId="generoInput">
           <Form.Label>Género</Form.Label>
-          <Form.Select {...register("genero")}>
+          <Form.Select {...register("genero")} isInvalid={errors.genero}>
             <option value="">Seleccione</option>
             <option value="1">Peticiones</option>
             <option value="2">Quejas</option>
@@ -102,7 +111,7 @@ const CreateAdminForm: FC = () => {
             <option value="4">Sugerencias</option>
           </Form.Select>
           <Form.Control.Feedback type="invalid">
-            Looks good!
+            {errors.genero?.message}
           </Form.Control.Feedback>
         </Form.Group>
       </Row>
@@ -110,7 +119,10 @@ const CreateAdminForm: FC = () => {
       <Row className="mb-3">
         <Form.Group as={Col} md="4" controlId="tipoSangreInput">
           <Form.Label>Tipo de sangre</Form.Label>
-          <Form.Select {...register("tipoSangre")}>
+          <Form.Select
+            {...register("tipoSangre")}
+            isInvalid={errors.tipoSangre}
+          >
             <option value="">Seleccione</option>
             <option value="1">Peticiones</option>
             <option value="2">Quejas</option>
@@ -118,13 +130,13 @@ const CreateAdminForm: FC = () => {
             <option value="4">Sugerencias</option>
           </Form.Select>
           <Form.Control.Feedback type="invalid">
-            Looks good!
+            {errors.tipoSangre?.message}
           </Form.Control.Feedback>
         </Form.Group>
 
         <Form.Group as={Col} md="4" controlId="rhInput">
           <Form.Label>RH</Form.Label>
-          <Form.Select {...register("rh")}>
+          <Form.Select {...register("rh")} isInvalid={errors.rh}>
             <option value="">Seleccione</option>
             <option value="1">Peticiones</option>
             <option value="2">Quejas</option>
@@ -132,7 +144,7 @@ const CreateAdminForm: FC = () => {
             <option value="4">Sugerencias</option>
           </Form.Select>
           <Form.Control.Feedback type="invalid">
-            Looks good!
+            {errors.rh?.message}
           </Form.Control.Feedback>
         </Form.Group>
 
@@ -140,11 +152,12 @@ const CreateAdminForm: FC = () => {
           <Form.Label>Teléfono</Form.Label>
           <Form.Control
             {...register("telefono")}
-            type="text"
+            isInvalid={errors.telefono}
+            type="number"
             placeholder="Ingrese su número de teléfono"
           />
           <Form.Control.Feedback type="invalid">
-            Looks good!
+            {errors.telefono?.message}
           </Form.Control.Feedback>
         </Form.Group>
       </Row>
@@ -154,19 +167,21 @@ const CreateAdminForm: FC = () => {
           <Form.Label>Correo electrónico</Form.Label>
           <Form.Control
             {...register("email")}
+            isInvalid={errors.email}
             type="text"
             placeholder="Ingrese su correo electrónico"
           />
           <Form.Control.Feedback type="invalid">
-            Looks good!
+            {errors.email?.message}
           </Form.Control.Feedback>
         </Form.Group>
 
         <Form.Group as={Col} md="4" controlId="claveInput">
           <Form.Label>Contraseña</Form.Label>
-          <InputGroup>
+          <InputGroup hasValidation>
             <Form.Control
               {...register("clave")}
+              isInvalid={errors.clave}
               type={showPassword ? "text" : "password"}
               placeholder="Ingrese su contraseña"
             />
@@ -178,7 +193,7 @@ const CreateAdminForm: FC = () => {
               )}
             </Button>
             <Form.Control.Feedback type="invalid">
-              Please choose a password.
+              {errors.clave?.message}
             </Form.Control.Feedback>
           </InputGroup>
         </Form.Group>
@@ -188,6 +203,7 @@ const CreateAdminForm: FC = () => {
           <InputGroup hasValidation>
             <Form.Control
               {...register("confirmarClave")}
+              isInvalid={errors.confirmarClave}
               type={showPasswordConfirmation ? "text" : "password"}
               placeholder="Ingrese su contraseña nuevamente"
             />
@@ -202,7 +218,7 @@ const CreateAdminForm: FC = () => {
               )}
             </Button>
             <Form.Control.Feedback type="invalid">
-              Please choose a password.
+              {errors.confirmarClave?.message}
             </Form.Control.Feedback>
           </InputGroup>
         </Form.Group>
