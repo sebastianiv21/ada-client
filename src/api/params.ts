8926b43989ae72127662b597c4481a595ae7916a
sebastianiv21 <seluti98@gmail.com>
2023-12-03 14:18:31 -0500

    feat: add sonner on params error

--- src/api/params.ts
+++ src/api/params.ts
@@ -1,16 +1,22 @@
 import api from "./server"
 import { type Params } from "@/types/paramsTypes"
 import { type QueryFunctionContext } from "@tanstack/react-query"
+import { toast } from "sonner"
 
 export const fetchParams = async (
   ctx: QueryFunctionContext,
 ): Promise<Params> => {
-  const [_, lista] = ctx.queryKey
-  const { data } = await api<Params>({
-    method: "GET",
-    url: "/parametros",
-    params: { lista },
-  })
+  try {
+    const [_, lista] = ctx.queryKey
+    const { data } = await api<Params>({
+      method: "GET",
+      url: "/parametros",
+      params: { lista },
+    })
 
-  return data
+    return data
+  } catch (error) {
+    toast.error("Error al cargar los parámetros")
+    throw error
+  }
 }
