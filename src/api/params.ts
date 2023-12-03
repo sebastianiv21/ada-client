99029adb4f3dd688c8368eefae12d54aa9f1cc7d
sebastianiv21 <seluti98@gmail.com>
2023-12-03 13:37:14 -0500

    fix: queryFn

--- src/api/params.ts
+++ src/api/params.ts
@@ -1,8 +1,10 @@
 import api from "./server"
 import { type Message } from "@/types/general"
 import { type Params } from "@/types/paramsTypes"
+import { type QueryFunctionContext } from "@tanstack/react-query"
 
-export const getParams = async (lista: string): Promise<Params | Message> => {
+export const fetchParams = async (ctx: QueryFunctionContext) => {
+  const [_, lista] = ctx.queryKey
   const { data } = await api<Params | Message>({
     method: "GET",
     url: "/parametros",
