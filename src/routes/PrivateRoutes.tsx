173ba8187699d94cedab04a0a62115e9074e5749
sebastianiv21 <seluti98@gmail.com>
2023-12-05 22:38:40 -0500

    fix(routes): route redirection

--- src/routes/PrivateRoutes.tsx
+++ src/routes/PrivateRoutes.tsx
@@ -4,7 +4,6 @@ import Navbar from "@/pages/public/components/Navbar/Navbar"
 import Footer from "@/pages/private/components/Footer/Footer"
 import Landing from "@/pages/public/Landing/Landing"
 import NotFound from "@/pages/NotFound"
-import NestedRoutes from "@/components/NestedRoutes"
 import { Container } from "react-bootstrap"
 import Contact from "@/pages/public/Contact/Contact"
 import Login from "@/pages/public/Login/Login"
@@ -14,21 +13,21 @@ import ChangePassword from "@/pages/public/ChangePassword/ChangePassword"
 
 const PrivateRoutes: FC = () => {
   return (
-    <NestedRoutes base="/app">
+    <>
       <Navbar />
       <Container>
         <Switch>
-          <Route path="/" component={Landing} />
-          <Route path="/contacto" component={Contact} />
+          <Route path="/app" component={Landing} />
+          <Route path="/app/contacto" component={Contact} />
           <Route path="/login" component={Login} />
           <Route path="/recuperar-clave" component={ForgotPassword} />
-          <Route path="/cambiar-clave/:token" component={ChangePassword} />
+          <Route path="/app/cambiar-clave/:token" component={ChangePassword} />
           <Route path="/crear-admin" component={CreateAdmin} />
           <Route component={NotFound} />
         </Switch>
       </Container>
       <Footer />
-    </NestedRoutes>
+    </>
   )
 }
 
--- src/routes/PublicRoutes.tsx
+++ src/routes/PublicRoutes.tsx
@@ -4,36 +4,30 @@ import Navbar from "@/pages/public/components/Navbar/Navbar"
 import Footer from "@/pages/public/components/Footer/Footer"
 import Landing from "@/pages/public/Landing/Landing"
 import NotFound from "@/pages/NotFound"
-import NestedRoutes from "@/components/NestedRoutes"
 import { Container } from "react-bootstrap"
 import Contact from "@/pages/public/Contact/Contact"
-// import PQRS from "@/pages/public/PQRS/PQRS"
 import Login from "@/pages/public/Login/Login"
 import ForgotPassword from "@/pages/public/ForgotPassword/ForgotPassword"
 import CreateAdmin from "@/pages/public/CreateAdmin/CreateAdmin"
 import ChangePassword from "@/pages/public/ChangePassword/ChangePassword"
-import PrivateRoutes from "./PrivateRoutes"
 
 const PublicRoutes: FC = () => {
   return (
-    <Route path="/:any*">
-      <NestedRoutes base="">
-        <Navbar />
-        <Container>
-          <Switch>
-            <Route path="/" component={Landing} />
-            {/* <Route path="/pqrs" component={PQRS} /> */}
-            <Route path="/contacto" component={Contact} />
-            <Route path="/login" component={Login} />
-            <Route path="/recuperar-clave" component={ForgotPassword} />
-            <Route path="/cambiar-clave/:token" component={ChangePassword} />
-            <Route path="/crear-admin" component={CreateAdmin} />
-            <Route component={NotFound} />
-          </Switch>
-        </Container>
-        <Footer />
-      </NestedRoutes>
-    </Route>
+    <>
+      <Navbar />
+      <Container>
+        <Switch>
+          <Route path="/" component={Landing} />
+          <Route path="/contacto" component={Contact} />
+          <Route path="/login" component={Login} />
+          <Route path="/recuperar-clave" component={ForgotPassword} />
+          <Route path="/cambiar-clave/:token" component={ChangePassword} />
+          <Route path="/crear-admin" component={CreateAdmin} />
+          <Route component={NotFound} />
+        </Switch>
+      </Container>
+      <Footer />
+    </>
   )
 }
 
